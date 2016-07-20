d57b4085d00b49532471e87bbb708aacad61402d
Jennifer <jrlawyer@gmail.com>
2016-07-20 11:54:42 -0400

    I added models.

---
+++ ElevenNote.Models/NoteEditModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    class NoteEditModel
+    {
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+    }
+}
+
