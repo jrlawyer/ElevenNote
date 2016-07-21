40a3ac3f2b3c3b2bb9e23cd9d6bb5941b987daa5
Jennifer <jrlawyer@gmail.com>
2016-07-21 14:36:23 -0400

    I added the connectivity and views for the note and details sections.

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,83 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {   //making service aware of user
+        private readonly Guid _userId;  //Identity uses guid to set user
+
+        public NoteService(Guid userId)  //constructor, takes userId and sets it as such
+        {
+            _userId = userId;
+        }
+
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var ctx = new ElevenNoteDbContext())
+            { //ctx = context
+                var query =
+                    ctx
+                        .Notes  //returning notes where ownerid is equal to userid, onwerid is the property of the note entity property
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                                e =>
+                                    new NoteListItemModel
+                                    {
+                                        NoteId = e.NoteId,
+                                        Title = e.Title,
+                                        CreatedUtc = e.CreatedUtc
+                                    }
+                             );
+
+                return query.ToArray();
+
+            }
+        }
+
+        public bool CreateNote(NoteCreateModel model)
+        {
+            var entity =  //converts model to database
+                new NoteEntity
+                {
+                    OwnerId = _userId,
+                    Title = model.Title, 
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.UtcNow
+                };
+            using (var ctx = new ElevenNoteDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+
+            }
+        }
+
+        public NoteDetailModel GetNoteById(int noteId)  //looking for a specific note by id
+        {
+            using (var ctx = new ElevenNoteDbContext()) //pulling data (entity) from database and converting it into NoteDetailModel
+            {
+                var entity = 
+                    ctx
+                            .Notes
+                            .Single(e => e.NoteId == noteId && e.OwnerId == _userId); //get note and authorize note
+                return
+                    new NoteDetailModel
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+
+        }
+
+    }
+}
---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,67 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    [Authorize]
+    public class NotesController : Controller
+    {
+        private readonly Lazy<NoteService> _svc;
+
+        public NotesController() //constructor + default
+        {
+            _svc = 
+                new Lazy<NoteService>(  //creates service when we actually need it, delays
+                    () =>
+                    {
+                        var userId = Guid.Parse(User.Identity.GetUserId());
+                        return new NoteService(userId);
+                    }
+              );
+        }
+
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var notes =
+                _svc.Value.GetNotes(); //referencing lazy from above
+
+            return View(notes);  //Webpage view references notes
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]  //user is sending info to us via the controller
+        [ValidateAntiForgeryToken]  //from the create.cshtml
+        public ActionResult Create(NoteCreateModel model)
+        {
+            if (!ModelState.IsValid) return View(model); //return view with data so that it isn't needing to be retyped, just corrected
+
+            if (!_svc.Value.CreateNote(model)) //if note is not created, generate string below and return to view
+                {
+                    ModelState.AddModelError("", "Unable to create note.");
+                    return View(model);
+                }
+
+            TempData["SaveResult"] = "Your note was created.";
+
+            return RedirectToAction("Index");  //returns to list page
+        }
+
+        public ActionResult Details(int id)  //controller will look at notescontroller, method of details, id is id from the route (note ID number)
+        {
+            var note = _svc.Value.GetNoteById(id);
+
+            return View(note);
+        }
+    }
+}
